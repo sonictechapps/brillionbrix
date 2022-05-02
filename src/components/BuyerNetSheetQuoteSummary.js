28b4af1f5904b8cc369e4f7aaf1e525afb6c414a
Shubhajit Paul <subhajit42@gmail.com>
2022-05-02 20:02:12 +0530

    commit chnages

--- src/components/BuyerNetSheetQuoteSummary.js
+++ src/components/BuyerNetSheetQuoteSummary.js
@@ -56,7 +56,7 @@ const BuyerNetSheetQuoteSummary = () => {
         // From HTML
         var finalY = doc.lastAutoTable.finalY || 10
         doc.setFontSize(14)
-        doc.text(`${getStingOnLanguage('TITLE_QUOTE_PROVIDED')} ${titleCompanyInfo.companyName}`, 50, finalY + 15)
+        doc.text(`${getStingOnLanguage('BUYER_ESTIMATE')} ${getStingOnLanguage('TITLE_QUOTE_PROVIDED')} ${titleCompanyInfo.companyName}`, 50, finalY + 15)
         doc.setFontSize(10)
         doc.setLineHeightFactor(2)
         doc.text(`${getStingOnLanguage('CREATED_ON')} - ${getCreateDate()}`, 70, finalY + 25)
@@ -215,7 +215,7 @@ const BuyerNetSheetQuoteSummary = () => {
                 <div className="row content">
                     <div className="col-sm-12 mt-3">
                         {titleCompanyInfo
-                            && <div><h2 className="labelstyle-quote">{getStingOnLanguage('TITLE_QUOTE_PROVIDED')} {titleCompanyInfo.companyName}. </h2></div>}
+                            && <div><h2 className="labelstyle-quote">{getStingOnLanguage('BUYER_ESTIMATE')} {getStingOnLanguage('TITLE_QUOTE_PROVIDED')} {titleCompanyInfo.companyName}. </h2></div>}
                         {quoteCreatedOn &&
                             <span className="question-style-output"> {getStingOnLanguage('CREATED_ON')} {getCreateDate()}</span>
                         }
